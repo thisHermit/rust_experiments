9b877cf18bbf2448ee7a95574c2510aab758bfaa
andrewrusso <and.russo.0511@gmail.com>
2025-03-13 00:52:55 +0100

    commit n°1 create saySomething

---
+++ saySomething/target/debug/deps/saySomething-fbe49a4e61847ad8.d
@@ -0,0 +1,5 @@
+/home/andrewrusso/RustroverProjects/rust_experiments/saySomething/target/debug/deps/saySomething-fbe49a4e61847ad8: src/main.rs
+
+/home/andrewrusso/RustroverProjects/rust_experiments/saySomething/target/debug/deps/saySomething-fbe49a4e61847ad8.d: src/main.rs
+
+src/main.rs:
---
+++ saySomething/target/debug/saySomething.d
@@ -0,0 +1,1 @@
+/home/andrewrusso/RustroverProjects/rust_experiments/saySomething/target/debug/saySomething: /home/andrewrusso/RustroverProjects/rust_experiments/saySomething/src/main.rs
