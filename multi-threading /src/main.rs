cbbfa4b92d768c609633c2a3895cc9e970d35801
Davide <davide.mattioli.2001@gmail.com>
2025-03-13 00:09:38 +0100

    Big Chungus part 4

--- multi-threading /src/main.rs
+++ multi-threading /src/main.rs
@@ -1,6 +1,10 @@
 use std::thread;
 use std::time::Duration;
 use rand::random;
+
+
+
+// Big Chungus Devs inc
 fn multi_threaded_fizz_buzz(){
     let handle = thread::spawn(|| {
         for i in 1..10 {
@@ -17,11 +21,7 @@ fn multi_threaded_fizz_buzz(){
     }
 }
 
-
-
-fn main() {
-    multi_threaded_fizz_buzz();
-
+fn chads1(){
     let mut chad = vec![ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ];
 
 
@@ -30,6 +30,22 @@ fn main() {
     let handle2 = thread::spawn(move || {for j in 0..chad.len() { chad[j as usize] = random() } chad });
     let chad = handle2.join().unwrap();
     println!("Is it still cool?  {chad:?}");
+}
+fn MTND(length:i8 ) -> Vec<i128> {
+    println!("MTND {length}");
+    return vec![random(), random(), random(), random(), random()];
+
+}
+
+
+
+fn main() {
+    multi_threaded_fizz_buzz();
+    chads1();
+    let output = MTND(1);
+    println!("now i have fun {{ output: {output:?}  }}") ;
+
+
 
 
 
