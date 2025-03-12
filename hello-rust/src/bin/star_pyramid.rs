bf6e66c57e5d18d5f50fdc6e4e9d3ecaa02951ef
Bashar Jaan Khan <basharjaankhan@gmail.com>
2025-03-12 23:00:17 +0100

    feat: add simple pyramid

--- hello-rust/src/bin/star_pyramid.rs
+++ hello-rust/src/bin/star_pyramid.rs
@@ -1,6 +1,9 @@
+use std::io::{self, BufRead};
+
 fn main() {
+    let stdin = io::stdin();
     let height = 5;
-    for i in 0..height {
+    for i in 1..height {
         for j in 0..i {
             print!("*");
         }
